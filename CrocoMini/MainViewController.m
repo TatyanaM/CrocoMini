7962a8556dcafa532ef9495894f32506b476694e
Tatiana Mudryak <TEMudryak.SBT@sberbank.ru>
2017-11-30 16:28:15 +0300

    init

---
+++ CrocoMini/MainViewController.m
@@ -0,0 +1,33 @@
+//
+//  MainViewController.m
+//  CrocoMini
+//
+//  Created by Tatiana Mudryak on 30/11/2017.
+//  Copyright © 2017 Tatiana Mudryak. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController () <UITabBarControllerDelegate>
+
+
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+
+}
+
+
+
+@end
---
+++ CrocoMini/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  CrocoMini
+//
+//  Created by Tatiana Mudryak on 30/11/2017.
+//  Copyright © 2017 Tatiana Mudryak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
